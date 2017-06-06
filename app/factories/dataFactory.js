77505bc18050bb8b0a18115286cca1bb737a02a6
Willie Pruitt <w_pruitt@hotmail.com>
2017-06-06 12:17:58 -0500

    filled function for dataFactory.js

--- app/factories/dataFactory.js
+++ app/factories/dataFactory.js
@@ -5,7 +5,19 @@ app.factory("DataFactory", function($q, $http, FBCreds) {
 //*************
 //PIN FUNCTIONS
 //*************
-const addPin = (newObj) => {
+const addPin = (pinObj) => {
+	return $q((resolve, reject) => {
+		$http.post(`${FBCreds.databaseURL}/pins.json`, object)
+		.then ((pinID) => {
+			resolve(pinID);
+		})
+		.catch((error) => {
+			reject(error);
+		});
+	});
+};
+
+const createPin = (newObj) => {
 	return $q((resolve, reject) => {
 		let object = JSON.stringify(newObj);
 		$http.post(`${FBCreds.databaseURL}/pins.json`, object)
@@ -18,12 +30,19 @@ const addPin = (newObj) => {
 	});
 };
 
-const createPin; //(pinObj)
-
 const postToFB;
 
-const editPin = (pinID, pinObj) => {
-	return $
+const editPin = (pinID, editedPinObj) => {
+	return $q((resolve, reject) => {
+		let newObj = JSON.stringify(editedPinObj);
+		$http.path(`${FBCreds.databaseURL}/pins/${pinID}.json`, newObj)
+		.then((pinObj) => {
+			resolve(pinObj);
+		})
+		.catch((error) => {
+			reject(error);
+		});
+	});
 }; //(pinID, pinObj)
 
 const getPinObj = (pinID) => {
@@ -62,24 +81,119 @@ const getPin = (pinID) => {
 	});
 };
 
-const getAllPins;
+const getAllPins = (user) => {
+	let pins = [];
+	return$q((resolve, reject) => {
+		$http.get(`${FBCreds.databaseURL}pins.json?orderBy="uid"&equalTo="${user}"`)
+		.then((pinObjs) => {
+			let pinCollection = pinObjs.data;
+			Object.keys(pinCollection).forEach((key) => {
+				pinCollection[key].id = key;
+				pins.push(pinCollection[key]);
+			});
+			resolve(pins);
+		})
+		.catch((error) => {
+			reject(error);
+		});
+	});
+};
 
 //***************
 //BOARD FUNCTIONS
 //***************
 
-const addBoard;
+const addBoard = (boardObj) => {
+	return $q((resolve, reject) => {
+		$http.post(`${FBCreds.databaseURL}/boards.json`, object)
+		.then ((boardID) => {
+			resolve(boardID);
+		})
+		.catch((error) => {
+			reject(error);
+		});
+	});
+};
 
-const editBoard;
+const editBoard = (boardID, editedboardObj) => {
+	return $q((resolve, reject) => {
+		let newObj = JSON.stringify(editedboardObj);
+		$http.path(`${FBCreds.databaseURL}/boards/${boardID}.json`, newObj)
+		.then((boardObj) => {
+			resolve(boardObj);
+		})
+		.catch((error) => {
+			reject(error);
+		});
+	});
+};
 
-const createBoard;
+const createBoard = (newObj) => {
+	return $q((resolve, reject) => {
+		let object = JSON.stringify(newObj);
+		$http.post(`${FBCreds.databaseURL}/boards.json`, object)
+		.then ((boardID) => {
+			resolve(boardID);
+		})
+		.catch((error) => {
+			reject(error);
+		});
+	});
+};
 
-const getBoardObj;
+const getBoardObj = (boardID) => {
+	return $q((resolve, reject) => {
+		$http.get(`${FBCreds.databaseURL}/boards/${boardID}.json`)
+		.then((boardObj) => {
+			resolve(boardObj.data);
+		})
+		.catch((error) => {
+			reject(error);
+		});
+	});
+};
 
-const delBoard; //(boardID)
+const delBoard = (boardID) => {
+	return $q((resolve, reject) => {
+		$http.delete(`${FBCreds.databaseURL}/boards/${boardID}.json`)
+		.then((reponse) => {
+			resolve(response);
+		})
+		.catch((error) => {
+			reject(error);
+		});
+	});
+};
 
-const getBoard;
 
-const getAllBoards; 
+const getBoard = (boardID) => {
+	return $q((resolve, reject) => {
+		$http.get(`${FBCreds.databaseURL}/boards/${boardID}.json`)
+		.then((boardObj) => {
+			resolve(boardObj.data);
+		})
+		.catch((error) => {
+			reject(error);
+		});
+	});
+};
+
+const getAllBoards = (user) => {
+	let boards = [];
+	return$q((resolve, reject) => {
+		$http.get(`${FBCreds.databaseURL}boards.json?orderBy="uid"&equalTo="${user}"`)
+		.then((boardObjs) => {
+			let boardCollection = boardObjs.data;
+			Object.keys(boardCollection).forEach((key) => {
+				boardCollection[key].id = key;
+				boards.push(boardCollection[key]);
+			});
+			resolve(boards);
+		})
+		.catch((error) => {
+			reject(error);
+		});
+	});
+}; 
 
 });
