979cd43d3183df849da6e137d7ce8d3f7cfb1be8
Willie Pruitt <w_pruitt@hotmail.com>
2017-06-06 13:21:54 -0500

    fixed dataFactory.js grunt issues

--- values/fb-creds.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-app.constant('FBCreds', {
- 	apiKey: "AIzaSyBKpio08H_PgPVNrdleYwE-slnrfSaMftk",
-    authDomain: "pin-the-tail.firebaseapp.com",
-    databaseURL: "https://pin-the-tail.firebaseio.com",
-
-  });
